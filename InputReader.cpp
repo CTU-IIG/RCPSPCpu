9dc8d01e7b610aab54bc28d01472a212e6b84a35
Libor Bukata <lbukata@gmail.com>
2012-02-13 18:20:32 +0100

    Add support for PROGEN/MAX 1.0 format.

--- InputReader.cpp
+++ InputReader.cpp
@@ -25,77 +25,127 @@ void InputReader::readFromStream(istream& IN) {
 	uint32_t shred;
 	string readedLine;
 
-	string::const_iterator sit;
-	string searchPattern1 = "- renewable";
-	string searchPattern2 = "MPM-Time";
-	string searchPattern3 = "#successors";
-	
-	// Read project basic parameters.
-	while (getline(IN,readedLine))	{
-		if ((sit = search(readedLine.begin(), readedLine.end(), searchPattern1.begin(), searchPattern1.end())) != readedLine.end())	{
-			string parsedNumber;
-			for (string::const_iterator it = sit; it != readedLine.end(); ++it)	{
-				if (isdigit(*it) > 0)
-					parsedNumber.push_back(*it);			
+	if (getline(IN, readedLine))	{
+		IN.seekg (0, ios::beg);
+		
+		if (!readedLine.empty() && readedLine[0] == '*')	{
+			/* PROGEN-SFX FORMAT */
+			string::const_iterator sit;
+			string searchPattern1 = "- renewable";
+			string searchPattern2 = "MPM-Time";
+			string searchPattern3 = "#successors";
+
+			// Read project basic parameters.
+			while (getline(IN,readedLine))	{
+				if ((sit = search(readedLine.begin(), readedLine.end(), searchPattern1.begin(), searchPattern1.end())) != readedLine.end())	{
+					string parsedNumber;
+					for (string::const_iterator it = sit; it != readedLine.end(); ++it)	{
+						if (isdigit(*it) > 0)
+							parsedNumber.push_back(*it);			
+					}
+					totalNumberOfResources = strToNumber(parsedNumber);
+				}
+
+				if ((sit = search(readedLine.begin(), readedLine.end(), searchPattern2.begin(), searchPattern2.end())) != readedLine.end())	{
+					IN>>shred>>numberOfActivities;
+					numberOfActivities += 2;
+				}
+
+				if ((sit = search(readedLine.begin(), readedLine.end(), searchPattern3.begin(), searchPattern3.end())) != readedLine.end())	{
+					break;
+				}
 			}
-			totalNumberOfResources = strToNumber(parsedNumber);
-		}
 
-		if ((sit = search(readedLine.begin(), readedLine.end(), searchPattern2.begin(), searchPattern2.end())) != readedLine.end())	{
-			IN>>shred>>numberOfActivities;
+			activitiesDuration = new uint32_t[numberOfActivities];
+			activitiesRequiredResources = new uint32_t*[numberOfActivities];
+			activitiesSuccessors = new uint32_t*[numberOfActivities];
+			activitiesNumberOfSuccessors = new uint32_t[numberOfActivities];
+			capacityOfResources = new uint32_t[totalNumberOfResources];
+
+			// Read succesors.
+			for (uint32_t activityId = 0; activityId < numberOfActivities; ++activityId)	{
+				size_t numberOfSuccessors;
+				IN>>shred>>shred>>numberOfSuccessors;
+
+				activitiesNumberOfSuccessors[activityId] = numberOfSuccessors;
+				activitiesSuccessors[activityId] = new uint32_t[numberOfSuccessors];
+
+				uint32_t successor;
+				for (uint32_t i = 0; i < numberOfSuccessors; ++i)	{
+					IN>>successor;
+					activitiesSuccessors[activityId][i] = successor-1;
+				}
+			}
+
+			for (uint32_t i = 0; i < 5; ++i)
+				getline(IN,readedLine);
+
+			// Read resource requirements.
+			for (uint32_t activityId = 0; activityId < numberOfActivities; ++activityId)	{
+				uint32_t activityDuration;
+				IN>>shred>>shred>>activityDuration;
+
+				activitiesDuration[activityId] = activityDuration;
+				activitiesRequiredResources[activityId] = new uint32_t[totalNumberOfResources];
+
+				for (uint32_t resourceId = 0; resourceId < totalNumberOfResources; ++resourceId)	{
+					uint32_t unitsReq; IN>>unitsReq;
+					activitiesRequiredResources[activityId][resourceId] = unitsReq;
+				}
+			}
+
+			for (uint32_t i = 0; i < 4; ++i)
+				getline(IN,readedLine);
+
+			// Read capacity of resources.
+			for (uint32_t resourceId = 0; resourceId < totalNumberOfResources; ++resourceId)	{
+				uint32_t resourceCapacity; IN>>resourceCapacity;
+				capacityOfResources[resourceId] = resourceCapacity;
+			}
+		} else if (!readedLine.empty())	{
+			/* PROGEN/MAX 1.0 FORMAT */
+			IN>>numberOfActivities>>totalNumberOfResources>>shred>>shred;
 			numberOfActivities += 2;
+
+			activitiesDuration = new uint32_t[numberOfActivities];
+			activitiesRequiredResources = new uint32_t*[numberOfActivities];
+			activitiesSuccessors = new uint32_t*[numberOfActivities];
+			activitiesNumberOfSuccessors = new uint32_t[numberOfActivities];
+			capacityOfResources = new uint32_t[totalNumberOfResources];
+
+			// Read activity successors.
+			for (uint32_t actId = 0; actId < numberOfActivities; ++actId)	{
+				uint32_t successor = 0;
+				size_t numberOfSuccessors = 0;
+				IN>>shred>>shred>>numberOfSuccessors;
+				activitiesNumberOfSuccessors[actId] = numberOfSuccessors;
+				activitiesSuccessors[actId] = new uint32_t[numberOfSuccessors];
+				for (uint32_t k = 0; k < numberOfSuccessors; ++k)	{
+					IN>>successor;
+					activitiesSuccessors[actId][k] = successor;
+				}
+			}
+
+			// Read activities resources requirement.
+			for (uint32_t actId = 0; actId < numberOfActivities; ++actId)	{
+				uint32_t resourceReq = 0;
+				uint32_t activityDuration = 0;
+				IN>>shred>>shred>>activityDuration;
+				activitiesDuration[actId] = activityDuration;
+				activitiesRequiredResources[actId] = new uint32_t[totalNumberOfResources];
+				for (uint32_t r = 0; r < totalNumberOfResources; ++r)	{
+					IN>>resourceReq;	
+					activitiesRequiredResources[actId][r] = resourceReq;
+				}
+			}	
+			
+			// Read resources capacity.
+			for (uint32_t r = 0; r < totalNumberOfResources; ++r)	{
+				uint32_t resourceCapacity = 0;
+				IN>>resourceCapacity;
+				capacityOfResources[r] = resourceCapacity;
+			}
 		}
-
-		if ((sit = search(readedLine.begin(), readedLine.end(), searchPattern3.begin(), searchPattern3.end())) != readedLine.end())	{
-			break;
-		}
-	}
-
-	activitiesDuration = new uint32_t[numberOfActivities];
-	activitiesRequiredResources = new uint32_t*[numberOfActivities];
-	activitiesSuccessors = new uint32_t*[numberOfActivities];
-	activitiesNumberOfSuccessors = new uint32_t[numberOfActivities];
-	capacityOfResources = new uint32_t[totalNumberOfResources];
-	
-	// Read succesors.
-	for (uint32_t activityId = 0; activityId < numberOfActivities; ++activityId)	{
-		size_t numberOfSuccessors;
-		IN>>shred>>shred>>numberOfSuccessors;
-
-		activitiesNumberOfSuccessors[activityId] = numberOfSuccessors;
-		activitiesSuccessors[activityId] = new uint32_t[numberOfSuccessors];
-
-		uint32_t successor;
-		for (uint32_t i = 0; i < numberOfSuccessors; ++i)	{
-			IN>>successor;
-			activitiesSuccessors[activityId][i] = successor-1;
-		}
-	}
-
-	for (uint32_t i = 0; i < 5; ++i)
-		getline(IN,readedLine);
-
-	// Read resource requirements.
-	for (uint32_t activityId = 0; activityId < numberOfActivities; ++activityId)	{
-		uint32_t activityDuration;
-		IN>>shred>>shred>>activityDuration;
-
-		activitiesDuration[activityId] = activityDuration;
-		activitiesRequiredResources[activityId] = new uint32_t[totalNumberOfResources];
-
-		for (uint32_t resourceId = 0; resourceId < totalNumberOfResources; ++resourceId)	{
-			uint32_t unitsReq; IN>>unitsReq;
-			activitiesRequiredResources[activityId][resourceId] = unitsReq;
-		}
-	}
-
-	for (uint32_t i = 0; i < 4; ++i)
-		getline(IN,readedLine);
-
-	// Read capacity of resources.
-	for (uint32_t resourceId = 0; resourceId < totalNumberOfResources; ++resourceId)	{
-		uint32_t resourceCapacity; IN>>resourceCapacity;
-		capacityOfResources[resourceId] = resourceCapacity;
 	}
 }
 
